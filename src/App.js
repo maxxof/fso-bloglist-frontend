c7d6d6dde77c612f7205d143b41c7dd4d0544efe
maxxof <maxim.egorov.soccer@gmail.com>
2022-12-01 15:59:11 +0200

    Ex 5.1 done

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null) 
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const blogsRenderer = () => (
+    <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null 
+        ?
+        <div>
+          <h2><b>log in to application</b></h2>
+          {loginForm()} 
+        </div>
+        :
+        <div>
+          <h2><b>blogs</b></h2>
+          <p>{user.name} logged in</p>
+          {blogsRenderer()}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
