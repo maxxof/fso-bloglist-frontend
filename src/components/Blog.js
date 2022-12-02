47952c868ca0d8bd1b480d473940cb13ba79e514
maxxof <maxim.egorov.soccer@gmail.com>
2022-12-02 20:11:00 +0200

    Ex 5.7 done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+  const [buttonLabel, setButtonLabel] = useState('view')
+  
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const toggleView = () => {
+    setFullView(!fullView)
+    fullView === true ?
+      setButtonLabel('view') :
+      setButtonLabel('hide')
+  }
+
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleView}>{buttonLabel}</button>
+      {fullView === true ?
+        <div>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes}<button>like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+        :
+        ''
+      }
+    </div>  
+  )
+}
 
 export default Blog
