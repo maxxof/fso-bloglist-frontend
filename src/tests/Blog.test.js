5ebdc7ee5be43045b79b3adbe0dd75043b9b7292
maxxof <maxim.egorov.soccer@gmail.com>
2022-12-05 13:37:02 +0200

    Ex 5.15 done

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -4,23 +4,10 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
-test('Blog renders blog`s title and author by default', () => {
-  const blog = {
-    title: 'title',
-    author: 'author',
-    url: 'url',
-    likes: 100
-  }
-  const { container } = render(<Blog blog={blog} />)
-  const element = screen.getByText('title author')
-  expect(element).toBeDefined()
-  const div = container.querySelector('.blog')
-  expect(div).not.toHaveTextContent('url')
-  expect(div).not.toHaveTextContent(100)
-})
+let blog
 
-test('Blog also renders blog´s url and likes when the view-button is clicked', async () => {
-  const blog = {
+beforeEach(() => {
+  blog = {
     title: 'title',
     author: 'author',
     url: 'url',
@@ -30,6 +17,20 @@ test('Blog also renders blog´s url and likes when the view-button is clicked', 
       name: 'max'
     }
   }
+})
+
+test('Blog renders blog`s title and author by default', () => {
+
+  const { container } = render(<Blog blog={blog} />)
+  const element = screen.getByText('title author')
+  expect(element).toBeDefined()
+  const div = container.querySelector('.blog')
+  expect(div).not.toHaveTextContent('url')
+  expect(div).not.toHaveTextContent(100)
+})
+
+test('Blog also renders blog´s url and likes when the view-button is clicked', async () => {
+
 
   const { container } = render(<Blog blog={blog} />)
   const user = userEvent.setup()
@@ -39,4 +40,19 @@ test('Blog also renders blog´s url and likes when the view-button is clicked', 
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent('url')
   expect(div).toHaveTextContent(100)
+})
+
+test('clicking the like button twice calls event handler twice', async () => {
+  const mockHandler = jest.fn()
+
+  render (<Blog blog={blog} updateBlog={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
