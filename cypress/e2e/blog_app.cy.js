fa5f4f8be1fbc1c8dede5ba2e4986c8e683fad81
maxxof <maxim.egorov.soccer@gmail.com>
2022-12-06 22:44:39 +0200

    Ex 5.17 done

---
+++ cypress/e2e/blog_app.cy.js
@@ -0,0 +1,19 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'max',
+      username: 'maxxof',
+      password: 'opsec'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.get('#username')
+    cy.get('#password')
+  })
+
+  
+})
